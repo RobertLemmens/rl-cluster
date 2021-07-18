c3a9e583a9066a779422e77d2a62358adfeb12b1
Robert Lemmens <rj.lemmens@pm.me>
2021-07-18 02:55:44 +0200

    added tekton

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 kubectl create namespace argocd
 kubectl create namespace rl-cluster
 kubectl create namespace ingress-nginx
+kubectl create namespace tekton-pipelines
 
 kubectl apply -f project.yaml
 kubectl apply -f cluster-root.yaml
