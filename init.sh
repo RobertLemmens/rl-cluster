86ec9aa60775b687e973d8ec0b91cfc5552b35d2
Robert Lemmens <rj.lemmens@pm.me>
2021-07-18 02:20:31 +0200

    added ingress nginx

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 kubectl create namespace argocd
 kubectl create namespace rl-cluster
+kubectl create namespace ingress-nginx
+
 kubectl apply -f project.yaml
 kubectl apply -f cluster-root.yaml
