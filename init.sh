8d56b54c741cb5015c4d8a117c52f449f50f1856
Robert Lemmens <rj.lemmens@pm.me>
2021-07-19 15:02:35 +0200

    added loki

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@ kubectl create namespace argocd
 kubectl create namespace rl-cluster
 kubectl create namespace ingress-nginx
 kubectl create namespace tekton-pipelines
+kubectl create namespace loki
+kubectl create namespace minio
 
 kubectl apply -f project.yaml
 kubectl apply -f cluster-root.yaml
