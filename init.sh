b7fcc0a9c1a5c7bd10b55e20767e973b9d6107fb
Robert Lemmens <rj.lemmens@pm.me>
2021-07-22 01:30:56 +0200

    enable minio console

--- init.sh
+++ init.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+# first insert secrets
+
 kubectl create namespace argocd
 kubectl create namespace rl-cluster
 kubectl create namespace ingress-nginx
 kubectl create namespace tekton-pipelines
 kubectl create namespace loki
 kubectl create namespace minio
+
+helm install argo-cd ./cluster-applications/argocd/charts/argo-cd -n argocd
+kubectl rollout status deployment argo-cd-argocd-server
 
 kubectl apply -f project.yaml
 kubectl apply -f cluster-root.yaml
