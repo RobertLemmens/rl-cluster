2ebebddd54e15e2b608658afed8e2ad1b51ea3b1
Robert Lemmens <rj.lemmens@pm.me>
2022-02-04 20:40:28 +0100

    update argocd version

--- init.sh
+++ init.sh
@@ -8,8 +8,9 @@ kubectl create namespace tekton-pipelines
 kubectl create namespace loki
 kubectl create namespace minio
 
+helm dependency update ./cluster-applications/argocd/charts/argo-cd
 helm install argo-cd ./cluster-applications/argocd/charts/argo-cd -n argocd
-kubectl rollout status deployment argo-cd-argocd-server
+kubectl rollout status deployment argo-cd-argocd-server -n argocd
 
-kubectl apply -f project.yaml
-kubectl apply -f cluster-root.yaml
+# kubectl apply -f project.yaml
+# kubectl apply -f cluster-root.yaml
