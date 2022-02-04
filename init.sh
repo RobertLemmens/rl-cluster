2b901b00f76ee15595893f81b18c60380f83f650
Robert Lemmens <rj.lemmens@pm.me>
2022-02-04 21:21:49 +0100

    updates

--- init.sh
+++ init.sh
@@ -12,5 +12,5 @@ helm dependency update ./cluster-applications/argocd/charts/argo-cd
 helm install argo-cd ./cluster-applications/argocd/charts/argo-cd -n argocd
 kubectl rollout status deployment argo-cd-argocd-server -n argocd
 
-# kubectl apply -f project.yaml
-# kubectl apply -f cluster-root.yaml
+kubectl apply -f project.yaml
+kubectl apply -f cluster-root.yaml
